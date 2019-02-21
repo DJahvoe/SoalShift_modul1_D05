c39dd656071e54b8ad78c3dd6eb1093c7bbcc951
DJahvoe <43902118+DJahvoe@users.noreply.github.com>
2019-02-21 20:28:55 +0700

    Delete 2b.sh

--- Problem2/2b.sh
+++
@@ -1,1 +0,0 @@
-awk -F "," '{if ($1 =="United States" && $7 =="2012")productline[$4]+=$10;}END{for (i in productline)print i", "productline[i];}' WA_Sales_Products_2012-14.csv | sort -t, -nk2 -r | head -3
